54efe5fd85d093980f37efd3861abc74e2f466d0
LuisCQueiroz <djlc2@yahoo.com.br>
2023-11-15 19:51:09 -0300

    Aulas
    Aulas realizadas

--- Descubra o Python/Cap. 02/classes_start.py
+++ Descubra o Python/Cap. 02/classes_start.py
@@ -1,3 +1,24 @@
 #
 # Exemplo de como criar classes
 #
+class minhaClasse():
+    def __init__(self):
+        self.meuAtributo = "Passou pelo Construtor !"
+
+    def meuMetodo(self):
+        print("Passou pelo meuMetodo!")
+
+    def meuMetodo2(self, valor):
+        self.outroAtributo = valor
+        print(self.outroAtributo)
+
+def criaObjeto():
+    meuObj = minhaClasse()
+    var1 = meuObj.meuAtributo
+    print(var1)
+
+    meuObj.meuMetodo()
+    meuObj.meuMetodo2("Executando um método !")
+
+criaObjeto()
+
--- Descubra o Python/Cap. 03/datas_start.py
+++ Descubra o Python/Cap. 03/datas_start.py
@@ -1,4 +1,27 @@
 #
 # Arquivo com exemplos de manipulação de  datas
 #
+from datetime import date
+from datetime import time
+from datetime import datetime
 
+def ManipulaDataHora():
+    hoje = date.today()
+    print("Hoje é dia: ", hoje)
+    print("Partes da data:")
+    print("Dia: ", hoje.day)
+    print("Mês: ", hoje.month)
+    print("Ano: ", hoje.year)
+    print("Número do dia da semana: ", hoje.weekday())
+    dias =["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira","Sexta-Feira", "Sábado", "Domingo"]
+    print("Dia da semana: ", dias[hoje.weekday()])
+
+    data = datetime.now()
+    print("Data e hora: " , data)
+
+    tempo = datetime.time(data)
+    print("Hora Atual: ", tempo)
+
+
+
+ManipulaDataHora()
