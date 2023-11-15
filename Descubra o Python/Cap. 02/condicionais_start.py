4e73fa81ef5db2223a25f0df1fac85566e89bae8
LuisCQueiroz <djlc2@yahoo.com.br>
2023-11-15 12:28:34 -0300

    Descubra o Python
    Descubra o Python
    
    Atualizações em sistema

---
+++ Descubra o Python/Cap. 02/condicionais_start.py
@@ -0,0 +1,17 @@
+#
+# Arquivo de exemplo das estruturas condicionais
+#
+def Condicionais():
+    x, y = 100, 100
+    
+    if(x<y):
+        print("X é menor que Y")
+    elif(x == y):
+        print("X é igual a Y")
+    else:
+        print("X é maior que Y")
+
+Condicionais()
+
+
+    
